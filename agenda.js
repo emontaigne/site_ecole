16fdca8e31d007ffe06d1d658425e9dc6e5e9e42
Aurélie Castro <acdemarly@gmail.com>
2020-09-28 16:23:14 +0200

    changed click to mouseover on popups

--- agenda.js
+++ agenda.js
@@ -1,21 +1,21 @@
 var div= document.getElementById("day");
 var popup=document.getElementById("popup");
 
-div.addEventListener("click", function(){
-    if(popup.style.display!=="block"){
+div.addEventListener("mouseover", function(){
         popup.style.display="block";
-    }else{
-        popup.style.display="none";
-    }
+})
+
+div.addEventListener("mouseout", function(){
+    popup.style.display="none";
 })
 
 var div= document.getElementById("other");
 var autre_popup=document.getElementById("autre_popup");
 
-div.addEventListener("click", function(){
-    if(autre_popup.style.display!=="block"){
+div.addEventListener("mouseover", function(){
+    
         autre_popup.style.display="block";
-    }else{
-        autre_popup.style.display="none";
-    }
+})
+div.addEventListener("mouseout", function(){
+    autre_popup.style.display="none";
 })
